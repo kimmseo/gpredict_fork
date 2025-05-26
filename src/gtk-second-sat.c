d34f032f2ff08d0dad6d069376f69a124c1f02c4
Minseo Kim <kimms010625@gmail.com>
2025-05-26 11:15:57 +0800

    Added pref-layout for world map + single sat + second sat

--- src/gtk-second-sat.c
+++ src/gtk-second-sat.c
@@ -347,7 +347,7 @@ static void update_field(GtkSecondSat * ssat, guint i)
         buff = vis_to_str(vis);
         break;
     case SECOND_SAT_FIELD_SKR:
-        buff = g_strdup_printf("%d", 0);
+        buff = g_strdup_printf("second sat %d", 0);
         break;
     default:
         sat_log_log(SAT_LOG_LEVEL_ERROR,
